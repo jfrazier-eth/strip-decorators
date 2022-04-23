4e49a2233fb0ddbecf1a7f5813c600b78f723661
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-04-22 22:24:24 -0500

    added scripts and all deps

--- compile.ts
+++ compile.ts
@@ -3,8 +3,15 @@ import { elideImports } from "./elide-imports";
 import * as tsConfigFull from './tsconfig.full.json';
 import * as tsConfigStripped from './tsconfig.stripped.json';
 import * as tsConfigBase from './tsconfig.json';
+import { copyFileSync, mkdirSync} from "fs";
+import {join} from "path";
 
 const entrypoint = './src/index.ts';
+
+mkdirSync(tsConfigStripped.compilerOptions.outDir)
+mkdirSync(tsConfigFull.compilerOptions.outDir)
+copyFileSync('./package.json', join(tsConfigStripped.compilerOptions.outDir, 'package.json'));
+copyFileSync('./package.json', join(tsConfigFull.compilerOptions.outDir, 'package.json'));
 
 const programFull = ts.createProgram({
   rootNames: [entrypoint],
